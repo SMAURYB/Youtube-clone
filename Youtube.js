0eb0fa815c2e904d0f989c28f01d4857bfe44fea
SMAURYB <sergiomauryb@gmail.com>
2022-02-13 11:32:06 -0500

    first commit

---
+++ Youtube.js
@@ -0,0 +1,12 @@
+// import {Cloudinary} from "@cloudinary/url-gen";
+// import {Transformation} from "@cloudinary/url-gen";
+
+
+// const cld = new Cloudinary({
+//     cloud: {
+//       cloudName: 'sergiomurybenedetti'
+//     }
+//   });
+  
+//   // cld.image returns a CloudinaryImage with the configuration set.
+//   const myImage = cld.image('sample'); 
