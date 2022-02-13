31dfe3acf0cb0a2d720d344ba35f8447f7f7eac5
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-02-13 12:36:41 -0500

    funcion del click y funcion de insertar texto en un h1

--- Youtube.js
+++ Youtube.js
@@ -1,9 +1,26 @@
-
 let inputSearch = document.getElementById('search');
 
-inputSearch.addEventListener('input', updateValue);
+// addEventListener(tipo de cambio que quieres escuchar, funcion que quieres ejecutar)
+inputSearch.addEventListener('input', updateValueInputHomePage);
 
-function updateValue(e) {
-  let res = e.srcElement.value;
-  console.log(res);
+//event es el cambio que recibe del addEventListener
+function updateValueInputHomePage(event) {
+  let eventoRecibido = event.srcElement.value;
+  console.log(eventoRecibido);
+}
+
+// Llamamos a la lupa, a su div
+let lupa = document.getElementById('lupe');
+
+lupa.addEventListener('click', lupeClick);
+
+function lupeClick () {
+  //inicializamos la variable text y le asignamos el valor del input
+  let text = inputSearch.value;
+  // consologgeamos el text para ver que valor tiene, en tagged templates (estudiar)
+  console.log(`Hola, lo que escribiste fue: ${text}`);
+  //Renderizar el valor del input en un h1 en el html
+  let textHTML = document.getElementById('text-inner');
+  // imprimir el valor en el h1
+  textHTML.innerHTML = text
 }
